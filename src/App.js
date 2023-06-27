1647d2d15aeacb234462ad049801666c3f3d4a12
buraktr03 <turanburak@gmail.com>
2023-06-27 22:04:42 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  //? useState i board da tutmamizin sebebi her seferinde square component indan donecegine asagidaki const ile butun square lerin state ini tek array de tutuyoruz
+  //! we use useState when remembering sth in the program
+  //? Array(9).fill(null) creates an array with nine elements and sets each of them to null
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    //? If the square is already filled, you will return in the handleClick function early—before it tries to update the board state.
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    //? mevcut kare durumunu (squares dizisini) kopyalayarak başlar
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      //? kopyanın i öğesini "X" olarak değiştirir
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
+    //? Son olarak, setSquares işlevine değiştirilmiş kopyayı geçerek, kare durumunun güncellenmesini sağlar
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </div>
+  );
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+export default Board;
