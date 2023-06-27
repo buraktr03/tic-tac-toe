f2f46a8fa9efeb331f8215b5277f6228562acd90
buraktr03 <turanburak@gmail.com>
2023-06-27 22:52:51 +0300

    game func added

--- src/App.js
+++ src/App.js
@@ -84,4 +84,17 @@ function calculateWinner(squares) {
   }
   return null;
 }
-export default Board;
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+export default Game;
